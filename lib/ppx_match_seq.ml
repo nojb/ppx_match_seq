b4c8f5b6e0c5e0e5fd8b151e02c30c8bf34f4668
Nicolas Ojeda Bär <n.oje.bar@gmail.com>
2022-04-02 17:46:22 +0200

    _

--- lib/ppx_match_seq.ml
+++ lib/ppx_match_seq.ml
@@ -4,6 +4,9 @@ module A = Ast_builder.Default
 
 let loc = Location.none
 
+let pnil ~loc =
+  A.ppat_construct ~loc (A.Located.lident ~loc "Seq.Nil") None
+
 let pcons pcar pcdr =
   A.ppat_construct ~loc (A.Located.lident ~loc "Seq.Cons")
     (Some (A.ppat_tuple ~loc [pcar; pcdr]))
@@ -20,21 +23,23 @@ let enone =
 let pnone =
   A.ppat_construct ~loc (A.Located.lident ~loc "None") None
 
+let __seq = "__seq"
+
 let expand_pattern p ?guard e =
-  A.pexp_match ~loc (A.eapply ~loc (A.evar ~loc "__seq") [A.eunit ~loc])
+  A.pexp_match ~loc (A.eapply ~loc (A.evar ~loc __seq) [A.eunit ~loc])
     [
-      A.case ~lhs:(pcons p (A.pvar ~loc "__seq")) ~guard ~rhs:e;
-      A.case ~lhs:(A.pvar ~loc "_") ~guard:None ~rhs:enone;
+      A.case ~lhs:(pcons p (A.pvar ~loc __seq)) ~guard ~rhs:e;
+      A.case ~lhs:(A.ppat_any ~loc) ~guard:None ~rhs:enone;
     ]
 
 (* shoudl expand to an expression of type a seq -> (b * seq) option *)
 let expand_case p e =
   match p.ppat_desc with
-  | Ppat_extension ({txt = "seq"; loc = _}, PStr [{pstr_desc = Pstr_value (Nonrecursive, [{pvb_pat; pvb_expr; _}]);
-                                                   pstr_loc = _}]) ->
-    A.pexp_match ~loc (A.eapply ~loc pvb_expr [A.evar ~loc "__seq"])
+  | Ppat_extension
+      ({txt = "seq"; loc = _}, PStr [{pstr_desc = Pstr_value (Nonrecursive, [{pvb_pat; pvb_expr; _}]); pstr_loc = _}]) ->
+    A.pexp_match ~loc (A.eapply ~loc pvb_expr [A.evar ~loc __seq])
       [
-        A.case ~lhs:(psome (A.ppat_tuple ~loc [pvb_pat; A.pvar ~loc "__seq"])) ~guard:None
+        A.case ~lhs:(psome (A.ppat_tuple ~loc [pvb_pat; A.pvar ~loc __seq])) ~guard:None
           ~rhs:e;
         A.case ~lhs:pnone ~guard:None ~rhs:enone;
       ]
@@ -45,13 +50,21 @@ let expand_case p e =
 
 let rec expand_case0 p e =
   match p.ppat_desc with
-  | Ppat_construct ({txt = Lident "::"; _}, Some ([], {ppat_desc = Ppat_tuple [p1; p2]; _})) ->
+  | Ppat_construct ({txt = Lident "::"; loc = _}, Some ([], {ppat_desc = Ppat_tuple [p1; p2]; _})) ->
     expand_case p1 (expand_case0 p2 e)
+  | Ppat_any ->
+    e
+  | Ppat_construct ({txt = Lident "[]"; loc = _}, None) ->
+    A.pexp_match ~loc (A.eapply ~loc (A.evar ~loc __seq) [A.eunit ~loc])
+      [
+        A.case ~lhs:(pnil ~loc) ~guard:None ~rhs:e;
+        A.case ~lhs:(A.ppat_any ~loc) ~guard:None ~rhs:enone;
+      ]
   | _ ->
-    expand_case p e
+    A.pexp_let ~loc Nonrecursive [A.value_binding ~loc ~pat:p ~expr:(A.evar ~loc __seq)] e
 
 let expand_case0 {pc_lhs; pc_guard = _; pc_rhs} =
-  let pc_rhs = esome (A.pexp_tuple ~loc [pc_rhs; A.evar ~loc "__seq"]) in
+  let pc_rhs = esome (A.pexp_tuple ~loc [pc_rhs; A.evar ~loc __seq]) in
   expand_case0 pc_lhs pc_rhs
 
 let rec expand_cases = function
@@ -60,19 +73,17 @@ let rec expand_cases = function
   | case :: cases ->
     A.pexp_match ~loc (expand_case0 case)
       [
-        A.case ~lhs:(A.ppat_alias ~loc (psome (A.pvar ~loc "_")) (Loc.make ~loc "__x")) ~guard:None ~rhs:(A.evar ~loc "__x");
+        A.case ~lhs:(A.ppat_alias ~loc (psome (A.ppat_any ~loc)) (Loc.make ~loc "x")) ~guard:None ~rhs:(A.evar ~loc "x");
         A.case ~lhs:pnone ~guard:None ~rhs:(expand_cases cases)
       ]
-
-let seq_var = "__seq"
 
 let expand ~ctxt:_ e =
   match e.pexp_desc with
   | Pexp_function cases ->
-    A.eabstract ~loc [A.pvar ~loc seq_var]
+    A.eabstract ~loc [A.pvar ~loc __seq]
       (expand_cases cases)
   | Pexp_match (e, cases) ->
-    A.pexp_let ~loc Nonrecursive [A.value_binding ~loc ~pat:(A.pvar ~loc seq_var) ~expr:e]
+    A.pexp_let ~loc Nonrecursive [A.value_binding ~loc ~pat:(A.pvar ~loc __seq) ~expr:e]
       (expand_cases cases)
   | _ -> assert false
 
--- test/main.ml
+++ test/main.ml
@@ -4,5 +4,7 @@ type tok = A of int | B | C
 
 let rec expr = fun seq ->
   match%seq seq with
-  | A n :: [%seq? B when false] -> n
-  | [%seq let x = expr] -> x
+  | A n :: [%seq? B when false] :: C :: [%seq let x = expr] :: [%seq let y = expr] :: _ -> x + y + n
+  | C :: C :: C :: _ -> 0
+  | [%seq let x = expr] :: _ -> x + 1
+  | [A 12; B] -> 32
